d0798021827f5c8112d399b259062b2404213ed1
xero-lib <jonathankurtz@protonmail.com>
2021-01-26 00:31:29 -0600

    Migrated apps to separate repositories

--- apps/rust2ex/test/rust2ex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Rust2exTest do
-  use ExUnit.Case
-  doctest Rust2ex
-
-  test "greets the world" do
-    assert Rust2ex.hello() == :world
-  end
-end
--- apps/turn_x/test/turn_x_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule TurnXTest do
-  use ExUnit.Case
-
-  test "ports are working" do
-    port = Port.open({:spawn, Path.join([:code.priv_dir(:turn_x), "turnx_vpx"])}, [:binary])
-    Port.command(port, "hello Erlang")
-    Port.close(port)
-  end
-end
